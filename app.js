0bbfee103692346577947e2b594eb53a931467fc
jaybarrios14 <barriosjay304@gmail.com>
2024-09-06 10:13:55 +0800

    createhtmlfile

---
+++ app.js
@@ -0,0 +1,7 @@
+let firstName = "Ariel";
+let  LastName = " Sajolga ";
+
+console.log("Hii my name is " + firstName + LastName + "a BSIT student")
+alert("Ariel Sajolga ")
+
+document.getElementById("text").innerHTML  = "Hii my name is " + firstName + LastName + "a BSIT student"
